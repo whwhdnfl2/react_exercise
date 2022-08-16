9c914f3a6da7e0c9f1986c7471357ce58dfcd54f
Cha minjun <whwhdnfl2@naver.com>
2022-08-17 01:58:36 +0900

    83 완료

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -28,10 +28,9 @@ const DUMMY_EXPENSES = [
 const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-  const addExpenseHandler = (expense) => {
-    setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses];
-    });
+  const addExpenseHandler = (expense) => {setExpenses((prevExpense => {
+    return [...prevExpense, expense]
+  }));
   };
 
   // return React.createElement(
---
+++ section6/01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+import Button from '../../UI/Button/Button';
+import './CourseInput.css';
+
+const CourseInput = props => {
+  const [isValid, setIsValid] = useState(true);
+
+  const [enteredValue, setEnteredValue] = useState('');
+
+  const goalInputChangeHandler = event => {
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = event => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
+        <input style={{borderColor: !isValid ? 'red' : 'black', backgroundColor: !isValid ? 'salmon' : 'transparent'}} type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
